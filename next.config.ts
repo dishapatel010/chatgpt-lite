09683f951289bdc1207af17428935fc3e0ec7f2e
Nikhil <71930916+dishapatel010@users.noreply.github.com>
2025-07-27 17:03:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: 'standalone',
   sassOptions: {
     // includePaths: [path.join(__dirname, 'styles')]
